c0f8691218be870cc4196f4b6931ae9fe47b8054
a.kulinskiy <a.kulinskiy@tinkoff.ru>
2021-07-25 11:57:12 +0300

    add solution for contains duplicate

--- array/remove-duplicates/main.rs
+++ array/remove-duplicates/main.rs
@@ -1,3 +1,5 @@
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/727/
+
 // Given an integer array nums sorted in non-decreasing order,
 // remove the duplicates in-place such that each unique element appears only once.
 // The relative order of the elements should be kept the same.
