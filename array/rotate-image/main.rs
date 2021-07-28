c133db11d600025a243d5a2308f530ba21a47954
a.kulinskiy <a.kulinskiy@tinkoff.ru>
2021-07-28 09:04:54 +0300

    added solution for rotate-image

---
+++ array/rotate-image/main.rs
@@ -0,0 +1,23 @@
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/770/
+
+// You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+// You have to rotate the image in-place, which means you have to modify the input 2D matrix directly.
+// DO NOT allocate another 2D matrix and do the rotation.
+//
+// Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+// Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+
+impl Solution {
+    pub fn rotate(matrix: &mut Vec<Vec<i32>>) {
+        let n = matrix[0].len();
+        for i in 0..((n as i32) / 2) as usize {
+            for j in i..((n - i - 1) as i32) as usize {
+                let temp = matrix[i][j];
+                matrix[i][j] = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = temp;
+            }
+        }
+    }
+}
